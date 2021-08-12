b368c63db0eee30b5bd5d433206b073ff81fef47
Kian Boon Koh <kianboonkoh@gmail.com>
2021-08-12 11:16:55 +0800

    update utils.py to include logging

--- styletransfer/utils.py
+++ styletransfer/utils.py
@@ -1,18 +1,16 @@
-from PIL import Image  # to read images
 import base64
-
-from PIL import Image
-from io import BytesIO
+import logging
 import re
+from io import BytesIO
 
 from django.conf import settings
-from django.utils import timezone
 from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.utils import timezone
+from PIL import Image
+
 
 def cropImageTargetCoordinate(root_img, box):
-
    # get dimensions of input image
-
     width, height = root_img.size
     bbox = bounding_box(width, height, box)
 
@@ -21,13 +19,12 @@ def cropImageTargetCoordinate(root_img, box):
 
     return result_img
 
-def bounding_box(original_width, original_height, box):
 
+def bounding_box(original_width, original_height, box):
     # Left coordinate = BoundingBox.Left * image width
     # Top coordinate = BoundingBox.Top * image height
     # Face width = BoundingBox.Width * image width
     # Face height = BoundingBox.Height * image height
-
     left = original_width * box['Left']
     top = original_height * box['Top']
     right = left + original_width * box['Width']
@@ -44,7 +41,8 @@ def image_to_base64(img):
         image_bytes = base64.b64encode(buffered.getvalue())
         return image_bytes.decode("utf-8")
 
-def base64_to_image(base64_str, image_path=None):
+
+def base64_to_image(base64_str, image_path=None) -> Image.Image:
     base64_data = re.sub('^data:image/.+;base64,', '', base64_str)
     byte_data = base64.b64decode(base64_data)
     image_data = BytesIO(byte_data)
@@ -54,16 +52,18 @@ def base64_to_image(base64_str, image_path=None):
     return img
 
 
-def storeImageIntoDB(selectedModel, raw_img, img_format = 'JPEG'):
-                        
+def storeImageIntoDB(selectedModel, raw_img, img_format='JPEG'):
+
     new_img = selectedModel()
     pil_img = base64_to_image(raw_img)
     img_io = BytesIO()
-    pil_img.save(img_io, format = img_format)
+    pil_img.save(img_io, format=img_format)
 
     cur_time = str(timezone.now()).split('.')[0]
-    new_img.file_path = InMemoryUploadedFile(img_io, field_name=None, name = cur_time + '.jpg', 
-        content_type='image/jpeg', size=img_io.tell, charset=None)
-
-    new_img.create_date = timezone.now()
-    new_img.save()
+    try:
+        new_img.file_path = InMemoryUploadedFile(img_io, field_name=None, name=cur_time + '.jpg',
+                                                 content_type='image/jpeg', size=img_io.tell, charset=None)
+        new_img.create_date = timezone.now()
+        new_img.save()
+    except Exception as e:
+        logging.error(e)
